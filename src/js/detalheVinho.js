f5310d483af3da333e5cb551557cc16c25d234c6
Felipe <felipecerboncinix@gmail.com>
2024-04-28 11:26:12 -0300

    Feito cada produto abrir corretamente na pagina de detalheVinho

--- src/js/detalheVinho.js
+++ src/js/detalheVinho.js
@@ -1,8 +1,31 @@
+let imgProdutoHMTL = document.getElementById('img-produto')
+let nomeProdutoHMTL = document.getElementById('nome-produto')
+let precoProdutoHMTL = document.getElementById('preco-produto')
+
 let quantidadeHTML = document.getElementById('quantidade')
 let valorTotalHTML = document.getElementById('valor-total')
 
+let imgProduto = ""
+let nomeProduto = ""
+let precoProduto = 0
+
 let quantidade = 1 // Valor padrão inicial de quantidade do produto
 let valorTotal = 0
+
+window.onload = function() {
+    imgProduto = localStorage.getItem('imgProdutoSelecionado')
+    nomeProduto = localStorage.getItem('nomeProdutoSelecionado')
+    precoProduto = parseFloat(localStorage.getItem('precoProdutoSelecionado'))
+
+    imgProdutoHMTL.src = imgProduto
+    nomeProdutoHMTL.textContent = nomeProduto
+    precoProdutoHMTL.textContent = `R$${precoProduto.toFixed(2)}`
+
+    valorTotal = precoProduto.toFixed(2)
+
+    quantidadeHTML.innerText = `${quantidade}`
+    valorTotalHTML.innerText = `R$${valorTotal}`
+};
 
 function comprar() {
      alert('Produto adicionado ao carrinho com sucesso')
@@ -11,7 +34,7 @@ function comprar() {
 function aumentarQuantidade() {
     quantidade++
 
-    valorTotal = quantidade * 109.9
+    valorTotal = quantidade * precoProduto
 
     quantidadeHTML.innerText = `${quantidade}`
     valorTotalHTML.innerText = `R$${valorTotal.toFixed(2)}`
@@ -21,7 +44,7 @@ function diminuirQuantidade() {
     if(quantidade > 1) {
         quantidade--
 
-        valorTotal = quantidade * 109.9
+        valorTotal = quantidade * precoProduto
 
         quantidadeHTML.innerText = `${quantidade}`
         valorTotalHTML.innerText = `R$${valorTotal.toFixed(2)}`
--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,24 @@ window.onload = function() {
         sessionStorage.setItem('visited', 'true');
     }
 };
+
+document.querySelectorAll('.produto-box button').forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        var produtoBox = e.target.parentElement;
+
+        var imgProduto = produtoBox.querySelector('img').src
+        var nomeProduto = produtoBox.querySelector('p').textContent
+        var precoProdutoTxt = produtoBox.querySelectorAll('p')[1].textContent
+
+        var precoPartes = precoProdutoTxt.split(" ")
+        var precoValor = precoPartes[3]
+
+        var preco = precoValor.replace('R$', '').replace(',', '.');
+
+        localStorage.setItem('imgProdutoSelecionado', imgProduto)
+        localStorage.setItem('nomeProdutoSelecionado', nomeProduto)
+        localStorage.setItem('precoProdutoSelecionado', preco)
+
+        window.location.href = '../html/detalheVinho.html'
+    });
+});
