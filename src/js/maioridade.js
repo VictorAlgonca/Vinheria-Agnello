f5445c6c65ca9478b81899f30f24b035235d9cb0
Felipe <felipecerboncinix@gmail.com>
2024-04-28 01:35:56 -0300

    Removido maioridade.js código

--- src/js/maioridade.js
+++ src/js/maioridade.js
@@ -1,9 +1,1 @@
-function permitido() {
-    alert("Acesso Permitido.")
-    // window.location.href = "index.html";
-}
 
-function negado() {
-    alert("Acesso Negado")
-    window.location.href = "https://www.google.com.br/?hl=pt-BR";
-}
